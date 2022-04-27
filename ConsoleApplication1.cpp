3fdc4906792de53fb8b7c9a1c65fa8ff4f9090e5
Давид Багирян <letynnchick@gmail.com>
2022-04-27 23:46:20 +0300

    third commit
    Поправил комментарии

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+#include "json_reader.h"
+
+int main() {
+    /*
+     * Примерная структура программы:
+     *
+     * Считать JSON из stdin
+     * Построить на его основе JSON базу данных транспортного справочника
+     * Выполнить запросы к справочнику, находящиеся в массиве "stat_requests", построив JSON-массив
+     * с ответами.
+     * Вывести в stdout ответы в виде JSON
+     */
+    std::ostringstream output;
+    json_reader::JsonReader processing(output);
+    processing.Reader();
+    std::cout << processing.Result().str();
+}
