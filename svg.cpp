d8600b3a2ec3df58b891f32f45aed020a0465c64
Давид Багирян <letynnchick@gmail.com>
2022-04-27 23:57:03 +0300

    Update svg.cpp
    Исправил комментарии

--- svg.cpp
+++ svg.cpp
@@ -32,7 +32,7 @@ namespace svg {
     void Object::Render(const RenderContext& context) const {
         context.RenderIndent();
 
-        // Делегируем вывод тега своим подклассам
+        // РґРµР»РµРіРёСЂСѓРµРј РІС‹РІРѕРґ С‚РµРіР° СЃРІРѕРёРј РїРѕРґРєР»Р°СЃСЃР°Рј
         RenderObject(context);
 
         context.out << std::endl;
@@ -54,7 +54,7 @@ namespace svg {
         auto& out = context.out;
         out << "<circle cx=\""sv << center_.x << "\" cy=\""sv << center_.y << "\" "sv;
         out << "r=\""sv << radius_ << "\""sv;
-        // Выводим атрибуты, унаследованные от PathProps
+        // РІС‹РІРѕРґРёРј Р°С‚СЂРёР±СѓС‚С‹, СѓРЅР°СЃР»РµРґРѕРІР°РЅРЅС‹Рµ РѕС‚ PathProps
         RenderAttrs(context.out);
         out << "/>"sv;
     }
@@ -99,7 +99,7 @@ namespace svg {
         if (font_family_ != "") out << "font-family=\""sv << font_family_ << "\""sv;
         if (font_family_ != "" && font_weight_ != "") out << " "sv;
         if (font_weight_ != "") out << "font-weight=\""sv << font_weight_ << "\""sv;
-        // Выводим атрибуты, унаследованные от PathProps
+        // РІС‹РІРѕРґРёРј Р°С‚СЂРёР±СѓС‚С‹, СѓРЅР°СЃР»РµРґРѕРІР°РЅРЅС‹Рµ РѕС‚ PathProps
         RenderAttrs(context.out);
         out << ">"sv;
         out << Metamorphosis() << "</text>"sv;
@@ -154,7 +154,7 @@ namespace svg {
                 out << " ";
             }
         }
-        // Выводим атрибуты, унаследованные от PathProps
+        // РІС‹РІРѕРґРёРј Р°С‚СЂРёР±СѓС‚С‹, СѓРЅР°СЃР»РµРґРѕРІР°РЅРЅС‹Рµ РѕС‚ PathProps
         RenderAttrs(context.out);
         out << " />"sv;
     }
