0b3e4ac3462242f587616b1f209c7c68a080e5b1
Давид Багирян <letynnchick@gmail.com>
2022-04-28 00:01:53 +0300

    Update domain.cpp

--- domain.cpp
+++ domain.cpp
@@ -7,15 +7,14 @@ Stop::Stop(std::string name, Coordinates coordinates)
 	, coordinates_(coordinates)
 {
 }
+bool Stop::operator==(const Stop& other) const {
+	return this->name_ == other.name_;
+}
+
 Bus::Bus(std::string name, std::vector<const Stop*> stops_of_bus, RouteType loop)
 	: name_(name), stops_of_bus_(stops_of_bus), loop_(loop)
 {
 }
-// для поиска остановки
-bool Stop::operator==(const Stop& other) const {
-	return this->name_ == other.name_;
-}
-// для поиска маршрута
 bool Bus::operator==(const Bus& other) const {
 	return this->name_ == other.name_;
 }
